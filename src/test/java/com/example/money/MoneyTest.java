a7b2b69f87cc13495d1758c8294186737737249c
sii <shio22a@gmail.com>
2022-03-30 17:10:29 +0900

    P14 Dollarの副作用をどうする？

--- src/test/java/com/example/money/MoneyTest.java
+++ src/test/java/com/example/money/MoneyTest.java
@@ -7,7 +7,7 @@ public class MoneyTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    five.times(3);
+    assertEquals(15, five.amount);
   }
 }
