ebc5c35d1abc4416a673a962cf594cbed99ca886
sii <shio22a@gmail.com>
2022-06-01 17:14:07 +0900

    P37 amountをMoneyに移動

--- src/main/java/com/example/money/Dollar.java
+++ src/main/java/com/example/money/Dollar.java
@@ -2,8 +2,6 @@ package com.example.money;
 
 public class Dollar extends Money {
 
-  private int amount;
-
   Dollar(int amount) {
     this.amount = amount;
   }
--- src/main/java/com/example/money/Money.java
+++ src/main/java/com/example/money/Money.java
@@ -1,5 +1,5 @@
 package com.example.money;
 
 public class Money {
-
+  protected int amount;
 }
