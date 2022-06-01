4f840abc1da90806102e2b5d193e9906f40a858d
sii <shio22a@gmail.com>
2022-06-01 17:18:18 +0900

    P39 Francのアサートを追加

--- src/test/java/com/example/money/MoneyTest.java
+++ src/test/java/com/example/money/MoneyTest.java
@@ -15,6 +15,9 @@ public class MoneyTest {
   public void testEquality() {
     assertTrue(new Dollar(5).equals(new Dollar(5)));
     assertTrue(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertTrue(new Franc(5).equals(new Franc(6)));
+
   }
 
   @Test
